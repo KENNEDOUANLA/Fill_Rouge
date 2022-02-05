695f3bfc4e2dd1a12d0ef73a8a6fed02f39cb155
simao2018 <chancelsim@outlook.fr>
2022-02-05 12:25:27 +0100

    add swagger to project

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
 const { USER } = require('./models/index');
 const { Route } = require("./routes");
+const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
 
-const cors = require("cors")
-const bodyParser = require("body-parser")
-const express = require("express")
+const swaggerDefinition = {
+    openapi: '3.0.1',
+    servers: [
+        {
+            url: 'http://localhost:80',
+            description: 'Local server'
+        }
+    ],
+    info: {
+        version: '1.0.0',
+        title: 'APIs Document',
+        description: 'Fil rouge description',
+        termsOfService: '',
+        contact: {
+            name: 'Fil rouge',
+        },
+        license: {
+            name: 'Apache 2.0',
+            url: 'https://www.apache.org/licenses/LICENSE-2.0.html'
+        }
+    }
+}
+
+const options = {
+    swaggerDefinition,
+    apis: ['./routes/index.js'],
+}
+
+const swaggerSpec = swaggerJSDoc(options);
+
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const express = require("express");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,8 +43,8 @@ app.use(bodyParser.json());
 app.use(cors());
 
 
-app.listen(process.env.PORT || 80, () => console.log("serveur on port :", process.env.PORT || 80));
+app.listen(80, () => console.log("serveur on port :", 80));
 app.use('', Route);
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 //USER.findAll().then((result) => console.log("result", result));
